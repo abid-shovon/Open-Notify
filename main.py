ffb578d7f27c06180776cbcdd5d561db836ac937
abid-shovon <shovonhasan333@gmail.com>
2025-06-03 23:29:22 +0600

    updated

---
+++ main.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+from datetime import datetime
+
+sender_email = "abidshovon945@gmail.com"
+receiver_email = "shovonhasan333@gmail.com"
+password = "zdky yvzu uqvj tsfy"
+
+now = datetime.now()
+time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+subject = "Laptop Alert: Laptop is Open"
+body = f"Your laptop is open now.\nTime: {time_str}"
+
+msg = MIMEText(body)
+msg["Subject"] = subject
+msg["From"] = sender_email
+msg["To"] = receiver_email
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
